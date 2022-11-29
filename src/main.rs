23213f2193850fd0be50c823721fc533d80130c3
Benoit Vermont <redwarp@gmail.com>
2022-11-29 08:49:49 +0100

    Run oxipng on release run
    + direct output to images folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::fs::create_dir_all;
 use std::fs::File;
 use std::io::Write;
@@ -54,10 +55,10 @@ fn draw(draw_fn: fn(&mut Canvas) -> (), output: &str, size: (i32, i32)) -> Resul
     draw_fn(canvas);
 
     // Save the result.
-    create_dir_all("rendering")
+    create_dir_all("images")
         .map_err(|_e| "ERROR: Coudn\'t create the `rendering` directory".to_string())?;
 
-    let file_name = format!("rendering/{}_{}x{}.png", output, width, height);
+    let file_name = format!("images/{}_{}x{}.png", output, width, height);
     let mut file = match File::create(file_name) {
         Err(_e) => {
             return Err(format!(
@@ -70,7 +71,7 @@ fn draw(draw_fn: fn(&mut Canvas) -> (), output: &str, size: (i32, i32)) -> Resul
     let image = surface.image_snapshot();
     match image.encode_to_data(EncodedImageFormat::PNG) {
         Some(data) => {
-            file.write_all(data.as_bytes())
+            file.write_all(&optimize_image(data.as_bytes())?)
                 .map_err(|_e| format!("ERROR: failed to write in the file `{}.png`", output))?;
         }
         None => {
@@ -87,4 +88,20 @@ impl Painting<'_> {
     fn new(draw_fn: fn(&mut Canvas) -> (), output: &str) -> Painting {
         Painting { draw_fn, output }
     }
+}
+
+#[cfg(not(debug_assertions))]
+fn optimize_image(png_data: &[u8]) -> Result<Cow<[u8]>, &str> {
+    let mut opts = oxipng::Options::from_preset(5);
+    opts.strip = oxipng::Headers::Safe;
+
+    oxipng::optimize_from_memory(png_data, &opts)
+        .map_err(|_e| "Couldn't optimize png")
+        .map(Cow::Owned)
+}
+
+#[cfg(debug_assertions)]
+fn optimize_image(png_data: &[u8]) -> Result<Cow<[u8]>, &str> {
+    // No-op
+    Ok(Cow::Borrowed(png_data))
 }
